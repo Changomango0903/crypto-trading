7df663faa2dbe04396bcf7f46459300738cca116
Sean Chang <schang09032003@gmail.com>
2024-06-25 05:40:01 +0100

    feat: Initialized Strategy Testing submodule

---
+++ backend/trading_strategy_testing/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class TradingStrategyTestingConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'trading_strategy_testing'
